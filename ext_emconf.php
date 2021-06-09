72705ea15fdbeb60622e3a62aedfade8d89f19e1
Philipp Kuhlmay <info@treupo.de>
2021-06-09 16:00:52 +0200

    Changed version of extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => true,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.4.6',
+    'version' => '1.0.0',
     'constraints' =>
         array(
             'depends' =>
