3d9d80a5d38301de56dcbcd748af3cfaf53b01c5
Philipp Kuhlmay <info@treupo.de>
2021-06-09 15:45:35 +0200

    [TASK] Reformats ext_emconf and updates EXT:vhs dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,32 +10,30 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-  'title' => 'MyFlat',
-  'description' => 'Flat manager, shows bookings of holiday flats in calendars, availability check included.',
-  'category' => 'plugin',
-  'author' => 'Joachim Ruhs',
-  'author_email' => 'postmaster@joachim-ruhs.de',
-  'state' => 'beta',
-  'uploadfolder' => true,
-  'createDirs' => '',
-  'clearCacheOnLoad' => 0,
-  'version' => '0.4.6',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-      'typo3' => '9.5.20-10.4.99',
-      'vhs' => '2.4.0',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  'clearcacheonload' => false,
-  'author_company' => NULL,
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'MyFlat',
+    'description' => 'Flat manager, shows bookings of holiday flats in calendars, availability check included.',
+    'category' => 'plugin',
+    'author' => 'Joachim Ruhs',
+    'author_email' => 'postmaster@joachim-ruhs.de',
+    'state' => 'beta',
+    'uploadfolder' => true,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.4.6',
+    'constraints' =>
+        array(
+            'depends' =>
+                array(
+                    'typo3' => '9.5.20-10.4.99',
+                    'vhs' => '6.4.0-6.9.99',
+                ),
+            'conflicts' =>
+                array(),
+            'suggests' =>
+                array(),
+        ),
+    'clearcacheonload' => false,
+    'author_company' => NULL,
 );
 
